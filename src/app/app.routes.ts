631066efac6da9ddd052bc7820fc7e30ae2eb295
Angel Mladenov <a.mladenov@seeburger.com>
2024-11-28 10:13:07 +0200

    ADd about page and home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {AboutComponent} from "./components/about/about.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {path: 'register', component: RegisterComponent},
+  {path: 'about', component: AboutComponent},
 ];
