2c687dcc3c4797681c4c75fbae99dcf8b21d62e1
Angel <48069264+velk20@users.noreply.github.com>
2024-12-06 16:07:28 +0200

    Update Offers

--- src/app/components/offers/offers.component.ts
+++ src/app/components/offers/offers.component.ts
@@ -1,36 +1,45 @@
 import { Component } from '@angular/core';
 import {OfferService} from "../../services/offer.service";
 import {Offer} from "../../models/offer";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {of} from "rxjs";
 import {ToastrService} from "ngx-toastr";
 import {CarService} from "../../services/car.service";
 import {Car} from "../../models/car";
+import {AuthService} from "../../services/auth.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-offers',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.css'
 })
 export class OffersComponent {
+  userId: number = 0;
   offers: Offer[] = [];
   cars: Car[] = [];
 
   constructor(private offerService: OfferService,
+              private authService: AuthService,
               private carService: CarService,
               private toastr: ToastrService,) {
   }
 
   ngOnInit() {
-    this.offerService.getAllOffersByUserId(1).subscribe(offers => {
+    this.authService.getUser().subscribe(user => {
+      this.userId = user.id;
+    })
+
+    this.offerService.getAllOffersByUserId(this.userId).subscribe(offers => {
       this.offers = offers.data as Offer[];
     });
 
-    this.carService.getAllCarsByUser(1).subscribe(cars => {
+    this.carService.getAllCarsByUser(this.userId).subscribe(cars => {
       this.cars = cars.data as Car[];
     });
 
